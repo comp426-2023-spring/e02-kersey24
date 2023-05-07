011a80c258c6bfd886961a873b7e577617040831
Ben Kersey <kersey24@ad.unc.edu>
2023-05-06 20:45:29 -0400

    test

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import {rps, rpsls} from './lib/rpsls.js';
+
 //// Load most basic dependencies
 // Create require function 
 // https://nodejs.org/docs/latest-v18.x/api/module.html#modulecreaterequirefilename
@@ -63,12 +65,64 @@ const app = express()
 const port = args.port || args.p || process.env.PORT || 8080
 // Load app middleware here to serve routes, accept data requests, etc.
 //
-// Create and update access log
-// The morgan format below is the Apache Foundation combined format but with ISO8601 dates
-app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
-    {stream: fs.createWriteStream(path.join(logpath, 'access.log')), flags: 'a' }
-))
-// Serve static files
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.listen(port);
+
+app.get('/app/', (req, res) => {
+	res.status(200).send('200 OK');
+})
+
+//Endpoint /app/rps/ that returns {"player":"(rock|paper|scissors)"}. (HINT: regex)
+
+app.get('/app/rps/', (req, res) => {
+    res.status(200).send(rps());
+})
+
+//Endpoint /app/rpsls/ that returns {"player":"(rock|paper|scissors|lizard|spock)"}.
+
+app.get('/app/rpsls/', (req, res) => {
+    res.status(200).send(rpsls());
+})
+
+//Endpoint /app/rps/play/ should accept request bodies in the following forms: shot=(rock|paper|scissors) (URLEncoded) or {"shot":"(rock|paper|scissors)"} (JSON) as data bodies and return {"player":"(rock|paper|scissors)","opponent":"(rock|paper|scissors)","result":"(win|lose|tie)"}.
+
+app.get('/app/rps/play/', (req, res) => {
+    res.status(200).send(rps(req.query.shot));
+})
+
+app.post('/app/rps/play/', (req, res) => {
+    res.status(200).send(rps(req.body.shot));
+})
+
+//Endpoint /app/rpsls/play/ should accept request bodies in the following forms: shot=(rock|paper|scissors) (URLEncoded) or {"shot":"(rock|paper|scissors)"} (JSON) and return {"player":"(rock|paper|scissors)","opponent":"(rock|paper|scissors)","result":"(win|lose|tie)"}
+
+
+app.get('/app/rpsls/play/', (req, res) => {
+    res.status(200).send(rpsls(req.query.shot));
+})
+
+app.post('/app/rpsls/play/', (req, res) => {
+    res.status(200).send(rpsls(req.body.shot));
+})
+
+//Endpoint /app/rpsls/play/(rock|paper|scissors)/ should return {"player":"(rock|paper|scissors)","opponent":"(rock|paper|scissors)","result":"(win|lose|tie)"}
+
+app.get('/app/rps/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.params.shot)));
+})
+
+//Endpoint /app/rpsls/play/(rock|paper|scissors|lizard|spock)/ should return {"player":"(rock|paper|scissors|lizard|spock)","opponent":"(rock|paper|scissors|lizard|spock)","result":"(win|lose|tie)"}
+
+app.get('/app/rpsls/play/:shot/', (req, res) => {
+    res.status(200).send(rpsls(req.params.shot));
+})
+
+//if endpoint not defined return 404
+
+app.get('*', (req, res) => {
+    res.status(404).send('404 NOT FOUND');
+})
 const staticpath = args.stat || args.s || process.env.STATICPATH || path.join(__dirname, 'public')
 app.use('/', express.static(staticpath))
 // Create app listener
