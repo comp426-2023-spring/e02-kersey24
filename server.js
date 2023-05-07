bfad49cfd2f25812cddb1268a997789c9b02ef23
Ben Kersey <97709332+kersey24@users.noreply.github.com>
2023-05-06 21:37:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -62,67 +62,70 @@ if (args.debug) {
 // Create an app server
 const app = express()
 // Set a port for the server to listen on
-const port = args.port || args.p || process.env.PORT || 5000
+const port = args.port || args.p || process.env.PORT || 8080
 // Load app middleware here to serve routes, accept data requests, etc.
 //
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.listen(port);
 
-app.get('/app/', (req, res) => {
-	res.status(200).send('200 OK');
-})
+app.get("/app/", (req, res, next) => {
+    res.json({"message":"200 OK"});
+	res.status(200);
+});
 
-//Endpoint /app/rps/ that returns {"player":"(rock|paper|scissors)"}. (HINT: regex)
+app.get("/app/rps", (req, res, next) => {
+    res.json(rps());
+	res.status(200);
+});
 
-app.get('/app/rps/', (req, res) => {
-    res.status(200).send(rps());
-})
+app.get("/app/rpsls", (req, res, next) => {
+    res.json(rpsls());
+	res.status(200);
+});
 
-//Endpoint /app/rpsls/ that returns {"player":"(rock|paper|scissors|lizard|spock)"}.
-
-app.get('/app/rpsls/', (req, res) => {
-    res.status(200).send(rpsls());
-})
-
-//Endpoint /app/rps/play/ should accept request bodies in the following forms: shot=(rock|paper|scissors) (URLEncoded) or {"shot":"(rock|paper|scissors)"} (JSON) as data bodies and return {"player":"(rock|paper|scissors)","opponent":"(rock|paper|scissors)","result":"(win|lose|tie)"}.
-
-app.get('/app/rps/play/', (req, res) => {
-    res.status(200).send(rps(req.query.shot));
-})
-
-app.post('/app/rps/play/', (req, res) => {
-    res.status(200).send(rps(req.body.shot));
-})
-
-//Endpoint /app/rpsls/play/ should accept request bodies in the following forms: shot=(rock|paper|scissors) (URLEncoded) or {"shot":"(rock|paper|scissors)"} (JSON) and return {"player":"(rock|paper|scissors)","opponent":"(rock|paper|scissors)","result":"(win|lose|tie)"}
+app.get("/app/rps/play", (req, res, next) => {
+    res.json(rps(req.query.shot));
+	res.status(200);
+});
 
 
-app.get('/app/rpsls/play/', (req, res) => {
-    res.status(200).send(rpsls(req.query.shot));
-})
+app.get("/app/rpsls/play", (req, res, next) => {
+    res.json(rpsls(req.query.shot));
+	res.status(200);
+});
 
-app.post('/app/rpsls/play/', (req, res) => {
-    res.status(200).send(rpsls(req.body.shot));
-})
 
-//Endpoint /app/rpsls/play/(rock|paper|scissors)/ should return {"player":"(rock|paper|scissors)","opponent":"(rock|paper|scissors)","result":"(win|lose|tie)"}
+app.post("/app/rps/play", (req, res, next) => {
+    res.json(rps(req.body.shot));
+	res.status(200);
+});
 
-app.get('/app/rps/play/:shot', (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.params.shot)));
-})
 
-//Endpoint /app/rpsls/play/(rock|paper|scissors|lizard|spock)/ should return {"player":"(rock|paper|scissors|lizard|spock)","opponent":"(rock|paper|scissors|lizard|spock)","result":"(win|lose|tie)"}
+app.post("/app/rpsls/play", (req, res, next) => {
+    res.json(rpsls(req.body.shot));
+	res.status(200);
+});
 
-app.get('/app/rpsls/play/:shot/', (req, res) => {
-    res.status(200).send(rpsls(req.params.shot));
-})
+app.get("/app/rps/play/:shot", (req, res, next) => {
+    res.json(rps(req.params['shot']));
+	res.status(200);
+});
 
-//if endpoint not defined return 404
 
-app.get('*', (req, res) => {
-    res.status(404).send('404 NOT FOUND');
-})
+app.get("/app/rpsls/play/:shot", (req, res, next) => {
+    res.json(rpsls(req.params.shot));
+	res.status(200);
+});
+
+
+app.all('/app/*', (req, res, next) => {
+	res.json({"message":"404 NOT FOUND"});
+    res.status(404);
+});
+// Create and update access log
+// The morgan format below is the Apache Foundation combined format but with ISO8601 dates
+app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
+    {stream: fs.createWriteStream(path.join(logpath, 'access.log')), flags: 'a' }
+))
+// Serve static files
 const staticpath = args.stat || args.s || process.env.STATICPATH || path.join(__dirname, 'public')
 app.use('/', express.static(staticpath))
 // Create app listener
